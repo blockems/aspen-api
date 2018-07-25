f14fc6c9782557d207a96880ce2314c4c43d1bce
blockems <block@as.net.au>
2018-07-25 17:02:27 +1000

    story model

--- create.js
+++ create.js
@@ -9,7 +9,7 @@ export function main(event, context, callback) {
   const data = JSON.parse(event.body);
 
   const params = {
-    TableName: "notes",
+    TableName: "story",
     // 'Item' contains the attributes of the item to be created
     // - 'userId': user identities are federated through the
     //             Cognito Identity Pool, we will use the identity id
